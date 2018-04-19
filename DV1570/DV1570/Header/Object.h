53ec9eb1ff958b14055e2af685ddaf65a2e9f354
Unknown <max.tlatlik94@gmail.com>
2018-04-19 22:58:31 +0200

    Testing .gitignore

---
+++ DV1570/DV1570/Header/Object.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef _OBJECT_H
+#define _OBJECT_H
+#include <SFML\Graphics.hpp>
+
+class Object
+{
+public:
+	Object();
+	Object(std::string pathToTexture, sf::IntRect area = sf::IntRect(), sf::Vector2f position = sf::Vector2f(0, 0));
+	~Object();
+	sf::Sprite* getSpritePtr();
+	void update();
+private:
+	sf::Vector2f m_pos;
+	sf::Texture m_texture;
+	sf::Sprite m_sprite;
+
+};
+#endif //!_OBJECT_H
+
