95f681d2d0d00e1b510418aa8e34e57f028e7d39
Velane92 <jakob.strand92@gmail.com>
2018-05-17 21:54:45 +0200

    Fixat lite med weapon
    Funderar på om man ska ha nåt form av luftmotstånd. inte svårt att fixa

---
+++ Weapon.cpp
@@ -0,0 +1,55 @@
+#include"weapon.h"
+
+void Weapon::normalize()
+{
+	float magnitude = sqrt((this->velocityVector.x * this->velocityVector.x) + (this->velocityVector.y * this->velocityVector.y));
+	this->normalizedVector.y = this->velocityVector.y / magnitude;
+	this->normalizedVector.x = this->velocityVector.x / magnitude;
+}
+
+Weapon::Weapon(sf::Vector2f pos)
+{
+	this->name = "?";
+	this->pos = pos;
+	this->shape = sf::CircleShape(5.0f);
+	this->shape.setFillColor(sf::Color::Red);
+	this->shape.setPosition(pos);
+	this->velocity = 0.0f;
+	this->velocityX = 0.0f;
+	this->velocityY = 0.0f;
+}
+
+void Weapon::shoot(float dt)
+{
+	//funderar på luftmotstånd här
+}
+
+void Weapon::setValues()
+{
+	float tempVelocity = 45.0f;
+	float tempAngle = 45.0f;
+	this->velocityX = tempVelocity * cos(tempAngle * PI / 180);
+	this->velocityY = tempVelocity * sin(tempAngle * PI / 180);
+
+	this->velocityVector.x = this->velocityX;
+	this->velocityVector.y = this->velocityY;
+	this->normalize();
+}
+
+void Weapon::update(float dt, sf::Vector2f pos)
+{
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+	{
+		this->shape.setPosition(pos);
+		this->setValues();
+
+	}
+	this->velocity = sqrt(pow(this->velocityX, 2) + pow(this->velocityY, 2));
+	this->shoot(dt);
+	this->shape.move(this->velocityX * dt, -this->velocityY * dt);
+}
+
+void Weapon::draw(sf::RenderTarget & target, sf::RenderStates & states) const
+{
+	target.draw(this->shape, states);
+}
---
+++ Weapon.h
@@ -0,0 +1,34 @@
+#ifndef WEAPON_H
+#define WEAPON_H
+#include<SFML\Graphics.hpp>
+#include<math.h>
+using namespace std;
+
+static const float GRAVITY = 9.82f;
+static const float PI = 3.14f;
+class Weapon : public sf::Drawable
+{
+private:
+	sf::Vector2f pos;
+	sf::CircleShape shape;
+
+	float velocity;
+	float velocityX;
+	float velocityY;
+	sf::Vector2f velocityVector;
+	sf::Vector2f normalizedVector;
+
+	void normalize();
+
+	string name;
+public:
+	Weapon(sf::Vector2f pos);
+
+	void shoot(float dt);
+	void setValues();
+	void update(float dt, sf::Vector2f pos);
+
+	void draw(sf::RenderTarget &target, sf::RenderStates &states) const;
+};
+#endif // !WEAPON_H
+
